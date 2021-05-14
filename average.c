bcd0270367523444fd70148774c097a56ab94d0d
kanishk singhal <72912708+kanix-1801@users.noreply.github.com>
2021-05-14 18:21:25 +0530

    Create average.c

---
+++ average.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+// fist i take some input (number) from usger 
+    long n1,n2,n3,n4,n5,sum;
+    float avg;
+    printf("enter n1 :");
+    scanf("%li" ,&n1);
+    printf("enter n2 :");
+    scanf("%li" ,&n2);
+    printf("enter n3 :");
+    scanf("%li" ,&n3);
+    printf("enter n4 :");
+    scanf("%li" ,&n4);
+    printf("enter n5 :");
+    scanf("%li" ,&n5);
+// i do sum also         
+    sum = n1+n2+n3+n4+n5;
+    printf("my sum is : %li\n" ,sum);
+// now my average is there ;)        
+    avg =(float)(n1+n2+n3+n4+n5)/2;       
+    printf("my average is : %f\n" ,avg);
+    return 0;
+}
